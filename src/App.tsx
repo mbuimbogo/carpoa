f62540a63791c9b3bdf3fd367a4f19780df47f53
mbuimbogo <mbuimbogo99@gmail.com>
2023-09-16 02:15:05 +0300

    Create a homepage and render it in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import HomePage from "./app/containers/HomePage";
+
+const AppContainer = styled.div`
+  ${tw`
+  w-full
+  h-full
+  flex
+  flex-col
+  `}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className='text-red-300'>Hello world</h1>
-    </div>
-  );
+  return <AppContainer>
+    <HomePage />
+  </AppContainer>;
 }
 
 export default App;
